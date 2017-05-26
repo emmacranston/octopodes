5dd200c791675c4c6f60a6f62a1f4b9aa6a3a4d5
emmacranston <cranston.emma@gmail.com>
2017-05-25 21:41:14 -0700

    added new method for nearby schools; refactored for consistent naming convention

--- lib/greatschools.py
+++ lib/greatschools.py
@@ -5,11 +5,11 @@ from urllib import urlencode, quote
 
 class greatschoolsClient():
 	def __init__(self, token):
-		if token: pass
+		if token: self._token = token
 		else: raise ValueError("No token value added")
 
-	def get(self, url):
-		request = Request(url, headers={"Accept": "application/json", "apikey" : key})
+	def __get(self, url):
+		request = Request(url)
 		contents = urlopen(request).read()
 		return contents
 
@@ -22,18 +22,32 @@ class greatschoolsClient():
 		schoolTypeList = ["public", "charter", "private"]
 		# also allowed are any items in the list separated by hyphens; gonna come back to this one later
 
-	def browseSchools(self, state = "CA", city = "San Francisco", schoolType = "", level = ""):
+	def schoolsBrowse(self, state = "CA", city = "San Francisco", schoolType = "", level = ""):
 		#make sure parameters for schoolType and level make sense
 		checkLevelParameter(level)
 		checkSchoolTypeParameter(schoolType)
 
 		#properly encode parameters
 		s = quote(state)
+		#custom replace function for city because greatschools does it... their own special way
 		c = city.replace("-", "_").replace(" ", "-")
 		st = quote(schoolType)
 		l = quote(level)
 
 		#put together request url
-		request = "http://api.greatschools.org/schools/%s/%s?key=%s" % (s, c, key)
+		request = "http://api.greatschools.org/schools/%s/%s?key=%s" % (s, c, self._token)
 		return request
-		#return urlopen(request).read()
+		#return self.__get(request)
+
+	def schoolsNearby(self, state, d = {}):
+		#define allowable key values for d
+		keyList = ["zip", "city", "address", "lat", "lon", "schoolType", "levelCode", "radius", "limit"]
+		for k in d.keys():
+			if k not in keyList:
+				print "Parameter Removed: '%s'" % k
+				d.pop(k)
+
+		#for some reason in this endpoint you can urlencode as per usual
+		params = "&" + urlencode(d)
+		request = "http://api.greatschools.org/schools/nearby?key=%s%s" % (self._token, params)
+		return request
