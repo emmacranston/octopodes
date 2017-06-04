f19d78f342d60e3a2918111438a338bd63e27dd1
emmacranston <cranston.emma@gmail.com>
2017-06-04 11:40:52 -0700

    Added inline documentation for onboard.py

--- docs/APIReference.md
+++ docs/APIReference.md
@@ -6,17 +6,42 @@ API Reference
 > 	OnboardClient gives you methods to use with the Onboard API. The 
 	methods defined are not all-inclusive of every available API call, but instead represent what may be necessary for this project.
 
-*	###__get###  
-	**Parameters**: url  
-	**Description**: Private method that returns an urllib2 request to the url. Simple.
-*	propertyId
-*	propertyDetail
-*	propertyZip
-*	propertySnapshot
-*	propertyZipSnapshot
-*	saleSnapshot
-*	propertyDetailMutable
+*	**__get**
+	*Parameters*: url  
+	*Description*: Private method that returns an urllib2 request to the url. Simple.
+*	**propertyId**
+     *Parameters*:
+     *Description*:
+*	**propertyDetail**
+    *Parameters*:
+    *Description*:
+*	**propertyZip**
+    *Parameters*:
+    *Description*:
+*	**propertySnapshot**
+    *Parameters*:
+    *Description*:
+*	**propertyZipSnapshot**
+    *Parameters*:
+    *Description*:
+*	**saleSnapshot**
+    *Parameters*:
+    *Description*:
+*	**propertyDetailMutable**
+    *Parameters*: d
+    *Description*: Creates an API call according to the key-value pairs assigned to the parameter ``d``. Acceptable key names include: 
+> 'minSaleAmt','maxSaleAmt', 'minTaxAmt', 'maxTaxAmt', 'minApprTtlValue', 'maxApprTtlValue', 'minAssdTtlValue', 'maxAssdTtlValue', 'minAVMValue', 'maxAVMValue', 'radius', 'cityName', 'startSaleTransDate', 'endSaleTransDate', 'lotSize1', 'lotSize2', 'minMktLandValue', 'maxMktLandValue','minMktTtlValue', 'maxMktTtlValue', 'GeoID', 'ID', 'postalCode', 'address', 
+> 'startSaleSearchDate', 'endSaleSearchDate', 'propertyType', 'minBathsTotal', 'maxBathsTotal', 'minBeds', 'maxBeds', 'universalSize'
 
->	All Onboard API documentation is available on their website: https://developer.onboard-apis.com/docs   
+
+>All Onboard API documentation is available on their [website](https://developer.onboard-apis.com/docs). 
 
 > From their developer site: "The data provided in the Onboard Property API is from CoreLogic and derived from county public records. It includes the most up-to-date data publicly available from the counties that publish it." I do not personally guarantee the accuracy or efficacy of this information.
+
+
+## zillowClient(): ##
+> Instantiates a client for use with the Zillow API. 
+> 
+
+## greatSchoolsClient():##
+> Instantiates a client for use with the GreatSchools API. For direct access to Greatschools data, apply for an API key with Greatschools [here](https://www.greatschools.org/api/registration.page). Otherwise, please use data files within the github folder.
--- lib/apis/onboard.py
+++ lib/apis/onboard.py
@@ -3,53 +3,58 @@ from urllib2 import Request, urlopen, URLError
 from urllib import urlencode
 
 class onboardClient():
+	"""Instantiates a client for use with the Onboard API. Requires an API token in string form."""
 	def __init__(self, token):
+		"""Constructor for Onboard Client"""
 		if token: self._token = token
 		else: raise ValueError("No token entered")
 
 	def __get(self, url):
-		request = Request(url, headers={"Accept": "application/json", "apikey" : self.token})
+		"""Gets a specified URL with the correct headers; uses XML format"""
+		request = Request(url, headers={"Accept": "application/xml", "apikey" : self.token})
 		contents = urlopen(request).read()
 
 		return contents
 
-	# property information methods, limited search parameter options as shown in the Onboard API docs
+# property information methods, limited search parameter options as shown in the Onboard API docs
 
 	def propertyId(self, postalCode = 91711, minBeds = 0, maxBeds = 100): 
-		#returns list of properties which fit the criteria within the search engine geoId
+		"""Returns XML list of properties which fit the criteria within the search engine geoId"""
 		response = "https://search.onboard-apis.com/propertyapi/v1.0.0/property/id?postalcode=%s&minBeds=%s&maxBeds=%s" % (postalCode, minBeds, maxBeds)
 		return self.__get(response)
 
 	def propertyDetail(self, id):
-		#returns detail on a specific property
+		"""Returns detail on a specific property defined by its Onboard ID"""
 		response = "https://search.onboard-apis.com/propertyapi/v1.0.0/property/detail?id=%s" % id
 		return self.__get(response)
 
 	def propertyZip(self, postalCode = 91711, propertyType = "RESIDENTIAL(NEC)"):
-		#should return list of properties within a zip
+		"""Returns list of properties within a zip, optional propertyType"""
 		response = "https://search.onboard-apis.com/propertyapi/v1.0.0/property/address?postalcode=%s&propertytype=%s&orderby=publisheddate&page=1&pagesize=100" % (postalCode, propertyType)
 		return self.__get(response)
 
 	def propertySnapshot(self, postalCode, minLotSize=0, maxLotSize=100000):
-		#returns properties which fit the criteria
+		"""Returns properties within postal code which fit the lot size criteria."""
 		response = "https://search.onboard-apis.com/propertyapi/v1.0.0/property/snapshot?postalcode=%s&minLotSize1=%s&maxLotSize1=%s" % (cityName, minLotSize, maxLotSize)
 		return self.__get(response)
 
 	def propertyZipSnapshot(self, postalCode, minAvmValue=0, maxAvmValue=1000000):
-		#returns list of properties within postal code which fit the criteria
+		"""Returns list of properties within postal code which fit your valuation criteria."""
 		response = "https://search.onboard-apis.com/propertyapi/v1.0.0/property/snapshot?postalcode=%s&minavmvalue=%s&maxavmvalue=%s" % (postalCode, minAvmValue, maxAvmValue)
 		return self.__get(response)
 
 	#sales information methods
 
-	def saleSnapshot(self, postalCode, minSaleAmount, maxSaleAmount, startDate, endDate):
-		#should return list of properties within geographical area which fit the criteria
+	def saleSnapshot(self, postalCode, maxSaleAmount, startDate, endDate, minSaleAmount = 0):
+		"""Returns list of properties within geographical area which fit your criteria. startDate and endDate must be in YYYY-MM-DD format."""
+
 		response = "https://search.onboard-apis.com/propertyapi/v1.0.0/sale/snapshot?postalcode=%s&minsaleamt=%s&maxsaleamt=%s&startsalesearchdate=%s&endsalesearchdate=%s"
 		return self.__get(response)
 
 	#mutable request- can use (almost) any available 'advanced' search parameters for the property resource in Onboard's API
 	#available parameters shown on keyList, explanations available through Onboard's docs.
 	def propertyDetailMutable(self, d = {}):
+		"""Creates an API call according to the key-value pairs assigned to the parameter ``d.`` See RE/docs/api.md for full list of acceptable key names."""
 		#list of acceptable key names for d
 		keyList = ['minSaleAmt','maxSaleAmt', 'minTaxAmt','maxTaxAmt','minApprTtlValue','maxApprTtlValue',
 			'minAssdTtlValue', 'maxAssdTtlValue', 'minAVMValue', 'maxAVMValue','radius','cityName','startSaleTransDate',
